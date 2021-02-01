fa1b1f20fb2f004fd25dbdb0a5e75a9fefb7180d
Ed Martins <ednilson.martins@caiena.net>
2021-02-01 16:39:46 -0300

    Revertendo funcionalidade "Adicionar Grupo"

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,4 @@
 class Group < ApplicationRecord
-  include Group::Search
-  include ::Sortable
-
   versionable
 
   belongs_to :covenant
@@ -13,8 +10,4 @@ class Group < ApplicationRecord
   validates_uniqueness_of :name, scope: :covenant_id
 
   accepts_nested_attributes_for :group_items, allow_destroy: true
-
-  def self.default_sort_column
-    'groups.name'
-  end
 end
